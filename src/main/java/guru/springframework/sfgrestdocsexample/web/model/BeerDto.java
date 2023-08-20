4434bec05296894eee25093e2fa14a51d5033a91
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2023-08-20 18:27:29 -0400

    s28-443 Documenting validation constraints

--- src/main/java/guru/springframework/sfgrestdocsexample/web/model/BeerDto.java
+++ src/main/java/guru/springframework/sfgrestdocsexample/web/model/BeerDto.java
@@ -1,9 +1,6 @@
 package guru.springframework.sfgrestdocsexample.web.model;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Null;
-import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -35,6 +32,7 @@ public class BeerDto {
     private OffsetDateTime lastModifiedDate;
 
     @NotBlank
+    @Size(min = 3, max = 100)
     private String beerName;
 
     @NotNull
@@ -48,6 +46,7 @@ public class BeerDto {
     @NotNull
     private BigDecimal price;
 
+    @Positive
     private Integer quantityOnHand;
 
 }
